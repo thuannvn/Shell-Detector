4e3a0d5611eab6ca9ced1c17cd26f2ed8f02eeb0
Thuan Nguyen <thuan.nguyen@8bitrockr.com>
2016-10-11 17:39:53 +0700

    Alert by email

--- shelldetect.py
+++ shelldetect.py
@@ -21,6 +21,10 @@ import urllib2
 import cgi
 import datetime
 
+# [START config]
+MAILGUN_DOMAIN_NAME = 'smtp.mailgun.org'
+MAILGUN_API_KEY = 'key-87ydupeg055ybas7hu5-w8m5fttk8td5'
+# [END config]
 
 class PhpSerializer(threading.Thread):
     def __init__(self):
@@ -307,6 +311,21 @@ class ShellDetector(threading.Thread):
 
         if _flag is True:
             self.output(_content, _class, _html)
+
+    # [START simple_message]
+    def send_simple_message(self, to):
+        url = 'https://api.mailgun.net/v3/{}/messages'.format(MAILGUN_DOMAIN_NAME)
+        auth = ('api', MAILGUN_API_KEY)
+        data = {
+            'from': 'Mailgun User <mailgun@{}>'.format(MAILGUN_DOMAIN_NAME),
+            'to': to,
+            'subject': 'Simple Mailgun Example',
+            'text': 'Plaintext content',
+        }
+
+        response = requests.post(url, auth=auth, data=data)
+        response.raise_for_status()
+    # [END simple_message]
 
     def supports_color(self):
         """
